29effda4cb83250b6d804a53fa76a51b90c8e4fb
Tanuj <tanujchoudhary001@gmail.com>
2020-08-17 19:45:43 +0530

    importing and loading css files

---
+++ src/js/styles.js
@@ -0,0 +1,11 @@
+//CSS files
+import "../css/globals.css";
+import "../css/header.css";
+import "../css/results.css";
+import "../css/recipe.css";
+import "../css/shopping.css";
+
+//Images
+import logo from "../img/logo.jpg";
+
+document.querySelector(".header__logo").src = logo;
