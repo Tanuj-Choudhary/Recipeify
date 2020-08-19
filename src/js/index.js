e86697fabce4adc1ed7298673e22cae4519e5c84
Tanuj <tanujchoudhary001@gmail.com>
2020-08-19 10:07:53 +0530

    Added pagination to controller

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,8 @@
 //All css or style handling
 import './styles';
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/utils';
 
 /** Global state of the app
  * Search object
@@ -14,23 +16,36 @@ const state = {};
 
 const ctrlSearch = async () => {
   // 1) Get query from the view
-  const query = 'burger'; //TODO
+  const query = searchView.getInput(); //TODO
 
   if (query) {
     // 2) New Search object and add it to state
     state.search = new Search(query);
 
     // 3) Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
 
     // 4) Search for recipes
     await state.search.getRecipes();
 
     // 5) Render the result on UI
-    console.log(state.search.recipes);
+    clearLoader();
+    searchView.renderRecipes(state.search.recipes);
   }
 };
 
-document.querySelector('.search').addEventListener('submit', (e) => {
+elements.searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   ctrlSearch();
+});
+
+elements.searchRecPages.addEventListener('click', (e) => {
+  const btn = e.target.closest('.btn-inline');
+  if (btn) {
+    const goToPage = btn.dataset.goto * 1;
+    searchView.clearResults();
+    searchView.renderRecipes(state.search.recipes, goToPage);
+  }
 });
