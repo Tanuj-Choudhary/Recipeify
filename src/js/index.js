3f9e376e2d2d1179eebf66fb849e56f09e7cc67c
Tanuj <tanujchoudhary001@gmail.com>
2020-08-19 20:24:21 +0530

    created recipe controller

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,9 @@
 //All css or style handling
 import './styles';
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/utils';
 
 /** Global state of the app
@@ -14,6 +16,9 @@ import { elements, renderLoader, clearLoader } from './views/utils';
  */
 const state = {};
 
+/**
+ * SEARCH CONTROLLER
+ */
 const ctrlSearch = async () => {
   // 1) Get query from the view
   const query = searchView.getInput(); //TODO
@@ -48,4 +53,62 @@ elements.searchRecPages.addEventListener('click', (e) => {
     searchView.clearResults();
     searchView.renderRecipes(state.search.recipes, goToPage);
   }
+});
+
+/**
+ * RECIPE CONTROLLER
+ */
+const ctrlRecipe = async () => {
+  // Get ID from url
+  const id = window.location.hash.replace('#', '');
+
+  if (id) {
+    //Prepare UI for changes
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
+
+    //Highlight selected
+    if (state.search) recipeView.highlightSelected(id);
+
+    // Create new recipe object
+    state.recipe = new Recipe(id);
+
+    //TESTING
+    window.r = state.recipe;
+
+    try {
+      // Get recipe data
+      await state.recipe.getRecipe();
+      state.recipe.parseIngredients();
+
+      // Calculate serving and time
+      state.recipe.calcServings();
+      state.recipe.calcTime();
+
+      // Render recipe
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
+    } catch (err) {
+      alert('Error processing recipe');
+    }
+  }
+};
+
+['hashchange', 'load'].forEach((event) =>
+  window.addEventListener(event, ctrlRecipe)
+);
+
+//Handling recipe Button clicks
+elements.recipe.addEventListener('click', (e) => {
+  if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+    // Decrease button is clicked
+    if (state.recipe.servings > 1) {
+      state.recipe.updateServings('dec');
+    }
+  } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+    // Increase button is clicked
+    state.recipe.updateServings('inc');
+  }
+  //Update view
+  recipeView.updateServingsAndIngredients(state.recipe);
 });
